324359a44dba19a3443f594475bd68acc644d078
BrenoAlberto <brenoaps435@gmail.com>
2024-06-18 10:43:48 +0000

    chore: fix venv activation alias

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -4,10 +4,10 @@ cd ${MAIN_DIR}
 
 if [ ! -d "venv" ]; then
     python -m venv venv
-    . ./venv/bin/activate
+    source ./venv/bin/activate
     pip install --upgrade pip
     pip install -r dev-requirements.txt
 else
-    . ./venv/bin/activate
+    source ./venv/bin/activate
     pip install -r dev-requirements.txt
 fi
