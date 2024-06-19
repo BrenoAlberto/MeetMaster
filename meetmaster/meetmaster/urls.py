338e67e5cc43bad6117252d404c150799a1d89f6
BrenoAlberto <brenoaps435@gmail.com>
2024-06-19 07:38:56 +0000

    feat: add Users urls

--- meetmaster/meetmaster/urls.py
+++ meetmaster/meetmaster/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("users.urls")),
     path("api/", include("events.urls")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
