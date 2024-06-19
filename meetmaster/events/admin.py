b7513e83149f685b22c1f9ca588abb3314b5f354
BrenoAlberto <brenoaps435@gmail.com>
2024-06-19 04:29:51 +0000

    feat: add EventAdmin

--- meetmaster/events/admin.py
+++ meetmaster/events/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Event
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    # list_display = list_display = [field.name for field in Event._meta.fields]
+    list_display = [field.name for field in Event._meta.fields if field.name not in ["created"]]
+    list_filter = [
+        "status",
+        "created",
+    ]
+    search_fields = ["title", "description"]
+    raw_id_fields = ["created_by"]
+    date_hierarchy = "date"
+    ordering = ["-date", "-time"]
+    show_facets = admin.ShowFacets.ALWAYS
