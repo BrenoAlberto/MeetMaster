862fef401742ffc1a758d30532a4a3616a4db8d7
BrenoAlberto <brenoaps435@gmail.com>
2024-06-19 16:24:55 +0000

    feat: ensure notification is sent on date change or cancellation

--- meetmaster/events/views.py
+++ meetmaster/events/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 
 from .models import Event
 from .serializers import EventSerializer
+from .tasks import send_notification
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -13,6 +14,46 @@ class EventViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
+
+    @action(detail=True, methods=["post"], url_path="change-date")
+    def change_date(self, request, pk=None):
+        event = self.get_object()
+        new_date = request.data.get("date")
+        new_time = request.data.get("time")
+
+        if not new_date or not new_time:
+            return Response(
+                {"detail": "Both date and time fields are required."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        event.date = new_date
+        event.time = new_time
+        event.full_clean()
+        event.save()
+
+        message = f"The date for the event '{event.title}' has been changed to {new_date}."
+        subject = "Event Date Change Notification"
+        send_notification.delay(event.id, message, subject)
+
+        return Response({"status": "Date changed and notification sent"}, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=["post"], url_path="cancel")
+    def cancel(self, request, pk=None):
+        event = self.get_object()
+
+        if event.status == Event.Status.CANCELED:
+            return Response(
+                {"detail": "This event has already been canceled."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        event.cancel()
+        message = f"The event '{event.title}' has been canceled."
+        subject = "Event Cancellation Notification"
+        send_notification.delay(event.id, message, subject)
+
+        return Response({"status": "Event canceled and notification sent"}, status=status.HTTP_200_OK)
 
     @action(detail=True, methods=["get"])
     def attendees(self, request, pk=None):
