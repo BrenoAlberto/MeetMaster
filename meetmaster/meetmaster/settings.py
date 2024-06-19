1973ec3c3105f04c9cfe2eed596c784be0848898
BrenoAlberto <brenoaps435@gmail.com>
2024-06-19 06:13:42 +0000

    feat: install users and update Events to use new CustomUser

--- meetmaster/meetmaster/settings.py
+++ meetmaster/meetmaster/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.postgres",
     "rest_framework",
+    "users",
     "events",
 ]
 
@@ -141,3 +142,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
+
+AUTH_USER_MODEL = "users.CustomUser"
