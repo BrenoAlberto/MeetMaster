7a5bff46e504d36697afcd7592957d220648552a
BrenoAlberto <brenoaps435@gmail.com>
2024-06-21 14:57:18 +0000

    refactor[events]: use EventCancelSerializer

--- meetmaster/events/serializers.py
+++ meetmaster/events/serializers.py
@@ -1,6 +1,5 @@
 from common.utils import model_data_prop_was_changed
 from django.utils import timezone
-from drf_spectacular.utils import OpenApiTypes, extend_schema_field
 from rest_framework import serializers
 
 from .models import Event
@@ -15,20 +14,14 @@ class EventSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Event
-        fields = "__all__"
+        exclude = ["attendees"]
         read_only_fields = ["created", "updated", "status", "total_attendees"]
 
     def update(self, instance, validated_data):
-        status_changed = model_data_prop_was_changed(instance, validated_data, "status")
         date_changed = model_data_prop_was_changed(instance, validated_data, "date")
         response = super().update(instance, validated_data)
-        self._notify(status_changed, date_changed, instance)
+        self._notify(date_changed, instance)
         return response
-
-    def validate_status(self, value):
-        if value != Event.Status.CANCELED:
-            raise serializers.ValidationError("Status can only be updated to 'Canceled'.")
-        return value
 
     def validate_date(self, value):
         current_datetime = timezone.now()
@@ -36,23 +29,28 @@ class EventSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Events cannot be created in the past.")
         return value
 
-    def _notify(self, status_changed, date_changed, instance):
-        if status_changed:
-            message = f"The event '{instance.title}' has been canceled."
-            subject = "Event Cancellation Notification"
-            print("Sending notification")
-            send_notification.delay(instance.id, message, subject)
+    def _notify(self, date_changed, instance):
         if date_changed:
             message = f"The date for the event '{instance.title}' has been changed to {instance.date}."
             subject = "Event Date Change Notification"
             send_notification.delay(instance.id, message, subject)
 
-    @extend_schema_field(OpenApiTypes.INT)
     def get_total_attendees(self, obj):
         return obj.attendees.count()
 
 
-class CreateEventSerializer(EventSerializer):
+class EventCancelSerializer(EventSerializer):
     class Meta:
         model = Event
-        fields = ["id", "title", "description", "date", "location", "status", "owner"]
+        fields = []
+
+    def update(self, instance, validated_data):
+        instance.status = Event.Status.CANCELED
+        instance.save()
+        self._notify(instance)
+        return instance
+
+    def _notify(self, event):
+        message = f"The event '{event.title}' has been canceled."
+        subject = "Event Cancellation Notification"
+        send_notification.delay(event.id, message, subject)
--- meetmaster/events/views.py
+++ meetmaster/events/views.py
@@ -1,50 +1,18 @@
 from common.permissions import IsAttendee, IsOwner
-from drf_spectacular.utils import OpenApiResponse, extend_schema, extend_schema_view
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Event
-from .serializers import CreateEventSerializer, EventSerializer
+from .serializers import EventCancelSerializer, EventSerializer
 
 
-@extend_schema_view(
-    list=extend_schema(
-        summary="List all events",
-        description="Retrieve a list of all events.",
-        responses={200: EventSerializer(many=True)},
-    ),
-    retrieve=extend_schema(
-        summary="Retrieve an event",
-        description="Retrieve detailed information about a specific event.",
-        responses={200: EventSerializer},
-    ),
-    create=extend_schema(
-        summary="Create a new event",
-        description="Create a new event with the specified details.",
-        request=EventSerializer,
-        responses={201: EventSerializer},
-    ),
-    update=extend_schema(
-        summary="Update an event",
-        description="Update details of an existing event.",
-        request=EventSerializer,
-        responses={200: EventSerializer},
-    ),
-    partial_update=extend_schema(
-        summary="Partially update an event",
-        description="Partially update details of an existing event.",
-        request=EventSerializer,
-        responses={200: EventSerializer},
-    ),
-    destroy=extend_schema(summary="Delete an event", description="Delete an existing event.", responses={204: None}),
-)
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
 
     def get_serializer_class(self):
-        if self.action == "create":
-            return CreateEventSerializer
+        if self.action == "cancel":
+            return EventCancelSerializer
         return EventSerializer
 
     def get_permissions(self):
@@ -55,7 +23,6 @@ class EventViewSet(viewsets.ModelViewSet):
             "update": [permissions.IsAuthenticated, IsOwner],
             "partial_update": [permissions.IsAuthenticated, IsOwner],
             "destroy": [permissions.IsAuthenticated, IsOwner],
-            "change_date": [permissions.IsAuthenticated, IsOwner],
             "cancel": [permissions.IsAuthenticated, IsOwner],
             "attendees": [permissions.IsAuthenticated, IsOwner | IsAttendee],
             "attende": [permissions.IsAuthenticated],
@@ -64,14 +31,6 @@ class EventViewSet(viewsets.ModelViewSet):
         self.permission_classes = permission_classes.get(self.action, [permissions.AllowAny])
         return super().get_permissions()
 
-    @extend_schema(
-        summary="List event attendees",
-        description="List all attendees for an event.",
-        responses={
-            200: OpenApiResponse(description="List of attendees"),
-            403: OpenApiResponse(description="Not authorized to view attendee details"),
-        },
-    )
     @action(detail=True, methods=["get"])
     def attendees(self, request, pk=None):
         event = self.get_object()
@@ -82,10 +41,6 @@ class EventViewSet(viewsets.ModelViewSet):
         else:
             return Response({"detail": "Not authorized to view attendee details"}, status=status.HTTP_403_FORBIDDEN)
 
-    @extend_schema(
-        summary="Add the current user as an attendee to the event.",
-        responses={200: OpenApiResponse(description="Attendee added")},
-    )
     @action(detail=True, methods=["post"])
     def attende(self, request, pk=None):
         event = self.get_object()
@@ -93,14 +48,19 @@ class EventViewSet(viewsets.ModelViewSet):
         event.save()
         return Response({"status": "attendee added"}, status=status.HTTP_200_OK)
 
-    @extend_schema(
-        summary="Remove an attendee from an event",
-        description="Remove the current user from the event's attendee list.",
-        responses={200: OpenApiResponse(description="Attendee removed")},
-    )
     @action(detail=True, methods=["post"])
     def remove_attendee(self, request, pk=None):
         event = self.get_object()
         event.attendees.remove(request.user)
         event.save()
         return Response({"status": "attendee removed"}, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=["post"])
+    def cancel(self, request, pk=None):
+        event = self.get_object()
+        if event.status == Event.Status.CANCELED:
+            return Response({"detail": "Event is already canceled."}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(event, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"status": "event canceled"}, status=status.HTTP_200_OK)
