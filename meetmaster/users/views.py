ec2976580e407f3c564c846046b4907d25ac4cf9
BrenoAlberto <brenoaps435@gmail.com>
2024-06-21 16:30:51 +0000

    chore[users]: remove drf-spectacular

--- meetmaster/users/views.py
+++ meetmaster/users/views.py
@@ -1,5 +1,4 @@
 from common.permissions import IsSuperUser, IsSuperUserOrSelf
-from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
@@ -7,59 +6,6 @@ from .models import CustomUser
 from .serializers import DetailedUserSerializer, PublicUserSerializer, UserCreateSerializer, UserUpdateSerializer
 
 
-@extend_schema_view(
-    list=extend_schema(
-        summary="List all users",
-        description="Only accessible by superusers.",
-        responses={
-            200: PublicUserSerializer(many=True),
-            403: None,
-        },
-    ),
-    retrieve=extend_schema(
-        summary="Retrieve a user",
-        description="Superusers can see detailed user info, others see public info.",
-        responses={
-            200: DetailedUserSerializer,
-            403: None,
-        },
-    ),
-    create=extend_schema(
-        summary="Create a new user",
-        description="Allows creation of a new user.",
-        request=UserCreateSerializer,
-        responses={
-            201: DetailedUserSerializer,
-            400: None,
-        },
-    ),
-    update=extend_schema(
-        summary="Update a user",
-        description="Superusers can update detailed user info, others can update their own info.",
-        request=UserUpdateSerializer,
-        responses={
-            200: UserUpdateSerializer,
-            403: None,
-        },
-    ),
-    partial_update=extend_schema(
-        summary="Partially update a user",
-        description="Superusers can partially update detailed user info, others can partially update their own info.",
-        request=UserUpdateSerializer,
-        responses={
-            200: UserUpdateSerializer,
-            403: None,
-        },
-    ),
-    destroy=extend_schema(
-        summary="Delete a user",
-        description="Superusers can delete any user, others can only delete their own account.",
-        responses={
-            204: None,
-            403: None,
-        },
-    ),
-)
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all().order_by("id")
 
